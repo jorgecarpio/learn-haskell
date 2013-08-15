b1b5ad7ce3b09083919957dba3ac0bc74ce589f8
jorgecarpio <jcarpio9@yahoo.com>
2013-08-15 12:24:07 -0700

    halve function

--- chap6.hs
+++ chap6.hs
@@ -128,3 +128,15 @@ merge [] xs = xs
 merge xs [] = xs
 merge (x:xs) (y:ys) | x <= y = x: merge xs (y:ys)
 					| otherwise = y: merge (x:xs) ys
+
+-- 5. Using merge, define a recursive function msort that implements
+-- merge sort, in which the empty list and singleton lists are already
+-- sorted, and any other list is sorted by merging together the
+-- two lists that result from sorting the two halves of the list
+-- separately.
+-- Hint: first define a function halve that splits a list into two
+-- halves whose length differs by at most one.
+
+halve :: [a] -> [([a],[a])]
+halve xs = [(take ((length xs `div` 2)) xs, drop ((length xs `div` 2)) xs)]
+
